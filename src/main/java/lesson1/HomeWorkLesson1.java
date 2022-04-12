f742f9cafec1c66a80e2af9375d207cfb0ef321a
KitaevIA <in62@bk.ru>
2022-04-12 23:46:08 +0300

    Home work 1

---
+++ src/main/java/lesson1/HomeWorkLesson1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class HomeWorkLesson1 {
+    public static void main(String[] args) {
+        boolean isSimple = true;
+        byte byteArg = -127;
+        int countStr = 1285;
+
+    }
+}
