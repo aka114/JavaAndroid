269c2f834b79c8be554ccb4995f6b3e17f86cc33
KitaevIA <in62@bk.ru>
2022-04-16 16:56:49 +0300

    Revert "Home work 2"
    This reverts commit 10471be85cc543f23e42edf3b2c285c931eb4243.

--- src/test/java/lesson2.java
+++
@@ -1,9 +0,0 @@
-public class lesson2 {
-    public static void main(String[] args) {
-        rewardEmployee("Вася");
-    }
-
-    private static void rewardEmployee(String name) {
-        System.out.println(name);
-    }
-}
