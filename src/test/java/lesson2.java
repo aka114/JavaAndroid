10471be85cc543f23e42edf3b2c285c931eb4243
KitaevIA <in62@bk.ru>
2022-04-16 16:07:02 +0300

    Home work 2

---
+++ src/test/java/lesson2.java
@@ -0,0 +1,9 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        rewardEmployee("Вася");
+    }
+
+    private static void rewardEmployee(String name) {
+        System.out.println(name);
+    }
+}
